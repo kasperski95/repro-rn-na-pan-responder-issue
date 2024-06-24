f535e6993da475807e41eb41142747318140a7e2
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2024-06-24 14:01:00 +0200

    add repro

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,118 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useRef, useState} from 'react';
+import {View, PanResponder, StyleSheet, Animated, Text} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const DraggableBoxExample: React.FC = () => {
+  const [panPositionAnim] = useState(new Animated.ValueXY());
+  const [isDragging, setIsDragging] = useState(false);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const panResponderRef = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        panPositionAnim.setOffset({
+          x: (panPositionAnim.x as any)._value,
+          y: (panPositionAnim.y as any)._value,
+        });
+        panPositionAnim.setValue({x: 0, y: 0});
+        setIsDragging(true);
+      },
+      onPanResponderMove: Animated.event(
+        [null, {dx: panPositionAnim.x, dy: panPositionAnim.y}],
+        {
+          useNativeDriver: false,
+        },
+      ),
+      onPanResponderRelease: () => {
+        panPositionAnim.flattenOffset();
+        Animated.spring(panPositionAnim, {
+          toValue: {x: 0, y: (panPositionAnim.y as any)._value},
+          useNativeDriver: false,
+        }).start();
+        setIsDragging(false);
+      },
+    }),
+  );
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponderRef.current.panHandlers}
         style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
+          styles.box,
+          panPositionAnim.getLayout(),
+          isDragging ? styles.drag : null,
         ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+        {!isDragging && <Text style={styles.text}>{'DRAG ME'}</Text>}
+      </Animated.View>
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingVertical: 128,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  box: {
+    width: 80,
+    height: 50,
+    borderBottomRightRadius: 50,
+    borderTopEndRadius: 50,
+    backgroundColor: 'red',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  drag: {
+    borderRadius: 50,
+    width: 50,
+    height: 50,
   },
-  highlight: {
-    fontWeight: '700',
+  text: {
+    flex: 1,
+    lineHeight: 50,
+    textAlign: 'center',
   },
 });
 
-export default App;
+export default DraggableBoxExample;
